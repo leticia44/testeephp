cba3cafc1da67006b346845ab45f331096c85b9f
12leticiasilva0990 <130669115+12leticiasilva0990@users.noreply.github.com>
2023-05-12 15:40:03 -0300

    first commit

---
+++ pagina1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //Declaração de variavel
+    $hora = date("H");
+
+
+    if ($hora < 12){
+        echo "<p>"  .$hora. "hs. Bom dia!</p>";
+    } else if($hora <18){
+        echo "<p>"  .$hora. "hs. Boa tarde! </p>";
+    } else {
+        echo "<p>"  .$hora. "hs. Boa noite!</p>";
+    }
+
+     ?> 
+</body> 
+</html>
